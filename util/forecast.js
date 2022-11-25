16736d5988aeb4f717e6091dcf72b13a2676b8a5
sadiqcodes2 <119068171+sadiqcodes2@users.noreply.github.com>
2022-11-25 22:08:41 +0530

    Update forecast.js

--- util/forecast.js
+++ util/forecast.js
@@ -1,7 +1,7 @@
 const req = require('request')
 
 const forecast=(latitude,longitude,location,cb)=>{
-    const url = "http://api.weatherstack.com/current?access_key=e1b8480d0f5a7c7d0f65b6506b03ddab&query="+latitude+','+longitude;
+    const url = "http://api.weatherstack.com/current?access_key=9aad2c1623c6b106c2e02ed8e0b4cfab&query="+latitude+','+longitude;
     
     req({ url, json: true }, (error, { body }) => {
         if (error) {
